0af2d9d84a5cb5b74b069ab7b01048bd41b74a73
Eduardo Sacramento de Sousa <eduardo.sacra.sousa@gmail.com>
2024-07-08 21:39:37 -0300

    alteracao rotas para contemplar 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,10 @@ export const routes: Routes = [
     {
         path: '',
         loadChildren: () => import('./modules/exercicio/exercicio.module').then( m=> m.ExercicioModule)
+    },
+    {
+      path: '**',
+      redirectTo: '',
+      pathMatch: 'full'
     }
 ];
